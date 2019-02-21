bfec108ecfb0e80db3dad834e5010041e1ce3223
Júlio Henderson <julio.henderson98@gmail.com>
2019-02-20 22:40:20 -0300

    fix plugin webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
             exclude: '/node_modules',
             query: {
                 presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spreed']
+                plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
